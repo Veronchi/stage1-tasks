9fc98d2436300379cd9221981fb8bb45b9fe6144
Veronchi <zhigunova1995@gmail.com>
2021-04-20 17:08:42 +0300

    added a change to the piano layout

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -27,18 +27,17 @@ modelPiano.elPiano.addEventListener('mousedown', function (event) {
 
 window.addEventListener('keydown', function (event) {
     let eventCodeStr = event.code;
-    let letterOfStr = eventCodeStr.substr(3, 1);
-    let datasetLetter = document.querySelector(`[data-letter="${letterOfStr}"]`);
+    if(eventCodeStr.startsWith("Key")) {
+        let letterOfStr = eventCodeStr.substr(3, 1);
+        let datasetLetter = document.querySelector(`[data-letter="${letterOfStr}"]`);
 
-    if (datasetLetter) {
-        const note = datasetLetter.dataset.note;
-        const src = `./assets/audio/${note}.mp3`;
-        playAudio(src);
-        getActiveBtn(datasetLetter);
-        // modelPiano.lastTargetElement = datasetLetter;  
+        if (datasetLetter) {
+            const note = datasetLetter.dataset.note;
+            const src = `./assets/audio/${note}.mp3`;
+            playAudio(src);
+            getActiveBtn(datasetLetter);
+        }
     }
-
-
 })
 
 window.addEventListener('keyup', function (event) {
@@ -80,4 +79,37 @@ function playAudio(src) {
 }
 
 
+let btnContainer = document.querySelector('.btn-container');
 
+btnContainer.addEventListener('click', function (event) {
+    let btnTarget = event.target;
+    let button = btnContainer.querySelectorAll('.btn')
+    let allPianoKey = document.querySelectorAll('.piano-key');
+
+    if (btnTarget.classList.contains('btn')) {
+        button.forEach((el) => {
+            if (el.classList.contains('btn-active')) {
+                el.classList.remove('btn-active');
+            }
+        });
+
+        btnTarget.classList.add('btn-active');
+        addClassLetter(allPianoKey);
+
+        if(btnTarget.classList.contains('btn-notes')) {
+            removeClassLetter(allPianoKey); 
+        }
+    }
+})
+
+function addClassLetter(pianoKey) {
+    pianoKey.forEach((el) => {
+        el.classList.add('piano-key-letter');
+    });
+}
+
+function removeClassLetter(pianoKey) {
+    pianoKey.forEach((el) => {
+        el.classList.remove('piano-key-letter');
+    });
+}
